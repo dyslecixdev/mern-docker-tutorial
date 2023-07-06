5291a0a6756cd311d8546d738233dd314c6d83f1
Christian Dean <christiandeandemesa@gmail.com>
2023-07-05 18:39:44 -0700

    final commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // Changes the port where the client is run from 5173 to 3000
+  server: {
+    host: true,
+    port: 3000,
+  },
+});
+
